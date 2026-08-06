08b7ff88ffddfd72aed300d1b50ffd8574d55c9e
OmitHasanAdor <ibneshams05@gmail.com>
2026-08-07 00:48:42 +0600

    header modified

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -4,12 +4,20 @@ import Image from 'next/image';
 import { format } from "date-fns";
 
 const Header = () => {
-    // date fns 
     return (
-        <div className=' pt-8 space-y-5 flex flex-col items-center justify-center '>
-            <h2><Image src={logo} alt="Logo" className=' ' /></h2>
-            <p className=" text-gray-500">Journalism Without Fear or Favour</p>
-            <p className=" text-gray-500">{format(new Date(), "EEEE, LLLL dd, yyyy")}</p>
+        <div className="pt-6 md:pt-8 pb-2 px-4 space-y-3 md:space-y-5 flex flex-col items-center justify-center text-center">
+            <Image
+                src={logo}
+                alt="Dragon News Logo"
+                className="w-55 sm:w-75 md:w-95 h-auto"
+                priority
+            />
+            <p className="text-sm md:text-base text-gray-500">
+                Journalism Without Fear or Favour
+            </p>
+            <p className="text-xs md:text-sm text-gray-400">
+                {format(new Date(), "EEEE, LLLL dd, yyyy")}
+            </p>
         </div>
     );
 };
